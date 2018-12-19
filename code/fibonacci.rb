819d5b6554f29c8f3da3b6582209e87861b5d2e0
eugenius123 <yugene123@gmail.com>
2018-12-19 15:20:21 -0500

    completed fibonacci using an iterative method that only saving the last 2 values to save space. Time complexity O(n) Space complexity O(1)

--- code/fibonacci.rb
+++ code/fibonacci.rb
@@ -1,6 +1,27 @@
 class Fibonacci
   # Implement a method that will calculate the Nth number of the Fibonacci
   # sequence (http://en.wikipedia.org/wiki/Fibonacci_number)
+  
+  # in order to calculate fibonacci in the most time and space efficient way,
+  # use an iterative solution that only keeps track of the current and previous values
+  # time complexity: O(n), space complexity O(1)
+
   def self.calculate(n)
+    prev = 0
+    curr = 1
+
+    if n == 0
+      prev
+    elsif n == 1
+      curr
+    else
+      (2..n).each do |fib|
+        mid = prev + curr
+        prev = curr
+        curr = mid
+      end
+      return curr
+    end
+
   end
 end
